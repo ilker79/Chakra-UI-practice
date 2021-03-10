c7429ad7e47520dc84c4354cf4cec26bd4f5e0e2
ilker <ilkerpeker79@yahoo.com>
2021-03-10 14:38:37 +0000

    started

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,14 @@
-import "./App.css";
+import React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
+  // 2. Use at the root of your app
   return (
-    <div className="App">
-      <h1>User Interface Libraries</h1>
-    </div>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   );
 }
 
